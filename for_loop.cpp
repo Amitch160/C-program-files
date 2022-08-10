44c7e444361f88f671c6a0995e7fda8d93b0cb51
Amit kumar <110763284+Amitch160@users.noreply.github.com>
2022-08-10 23:49:17 +0530

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int i,j=1;
+for(i=9;i==10,j<9;j++);
+{
+    printf("%d\n%d\n",i,j);
+    i++;
+}
+printf("out of loop");
+    return 0;
+}
