94d0934bd208edb4e3be9ce65ba0e14b52b23675
Amit kumar <110763284+Amitch160@users.noreply.github.com>
2022-08-17 21:52:03 +0530

    Add files via upload

---
+++ STATIC_GLOBAL_recursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int fun(int n){
+    static int x = 0;
+    if(n>0){
+        x++;
+        return fun(n-1)+x;
+
+    }
+    return 0;
+}
+
+int main(){
+    int a=5;
+    printf("%d",fun(a));
+    return 0;
+
+}
