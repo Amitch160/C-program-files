0cfe2f0eafdf16297959d5fb525fb7c9e397d840
Amit kumar <110763284+Amitch160@users.noreply.github.com>
2022-10-17 00:49:01 +0530

    Add files via upload

---
+++ stack1.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 100
+
+class Stack
+{
+
+    int top;
+
+public:
+    int a[MAX]; // maximun size of stack
+
+    Stack() { top = -1;}
+    bool push(int x);
+    int pop();
+    bool isEmpty();
+
+};
+
+bool Stack :: push(int x)
+{
+    if(top >= (MAX-1))
+    {
+        cout<< "Stack Overflow";
+        return false;
+
+    }
+    else
+    {
+        a[++top] = x;
+        cout << x <<"  pushed into stack\n";
+        return true;
+
+
+
+    }
+}
+
+int Stack :: pop()
+{
+    if(top < 0)
+    {
+        cout << "Stack Underflow";
+        return 0;
+
+    }
+    else 
+    {
+        int x = a[top-1];
+        return x;
+
+
+    }
+}
+
+bool Stack :: isEmpty()
+{
+    return (top<0);
+
+}
+
+//Driver program to test above functions
+
+int main()
+{
+    class Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << s.pop() << "  Popped from stack\n";
+
+    return 0;
+}
